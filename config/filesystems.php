763e1dc4e19032da561e30d8cac800f4208dbad2
Aneesh Ajithkumar <aaneeshaji@gmail.com>
2024-05-20 16:28:24 +0530

    Initial commit

--- config/filesystems.php
+++ config/filesystems.php
@@ -46,10 +46,10 @@ return [
 
         's3' => [
             'driver' => 's3',
-            'key' => 'AKIA2UC27NYBO3UBJUFS',
-            'secret' => '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',
-            'region' => 'ap-south-1',
-            'bucket' => 'laraveldata',
+            'key' => env('AWS_ACCESS_KEY_ID'),
+            'secret' => env('AWS_SECRET_ACCESS_KEY'),
+            'region' => env('AWS_DEFAULT_REGION'),
+            'bucket' => env('AWS_BUCKET'),
             'url' => env('AWS_URL'),
             'endpoint' => env('AWS_ENDPOINT'),
             'use_path_style_endpoint' => env('AWS_USE_PATH_STYLE_ENDPOINT', false),
