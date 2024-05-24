e919697f27a0bdad797e223e2f7aa3af2540a29a
Aneesh Ajithkumar <aaneeshaji@gmail.com>
2024-05-24 21:25:08 +0530

    Cron updates

--- routes/console.php
+++ routes/console.php
@@ -4,7 +4,12 @@ use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Schedule;
 
-Schedule::command('test:cron')->everySecond();
+//Schedule::command('test:cron')->everySecond();
+
+Schedule::command('test:cron')->everyFiveMinutes();
+
+// $schedule->command('mycommand:run')
+//     ->cron('0 0 * * 1,3,5'); // Runs at midnight (00:00) on Mondays, Wednesdays, and Fridays
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
