5e797824eb2bb2ac6d90776e9fc3d48d6eb519fc
Aneesh Ajithkumar <aaneeshaji@gmail.com>
2024-05-25 12:30:48 +0530

    Added new cronjob file for database backup process

--- routes/console.php
+++ routes/console.php
@@ -4,8 +4,10 @@ use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Schedule;
 
-Schedule::command('test:cron')->everyFiveMinutes();
+//Schedule::command('test:cron')->everyFiveMinutes();
 
-// $schedule->command('mycommand:run')
+Schedule::command('database-backup:cron')->everyFiveMinutes();
+
+// $schedule->command('database-backup:cron')
 //     ->cron('0 0 * * 1,3,5'); // Runs at midnight (00:00) on Mondays, Wednesdays, and Fridays
 
